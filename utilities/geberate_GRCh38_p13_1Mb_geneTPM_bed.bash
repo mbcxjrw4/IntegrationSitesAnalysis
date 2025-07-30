feda5fc33211d628e0d948ba0bcfb74d132f1e75
mbcxjrw4 <xiruowang@hotmail.com>
2025-07-30 17:24:55 +0100

    Delete utilities/geberate_GRCh38_p13_1Mb_geneTPM_bed.bash

--- utilities/geberate_GRCh38_p13_1Mb_geneTPM_bed.bash
+++
@@ -1,6 +0,0 @@
-# Find overlapping genes and sum their TPMs
-# make bed file containing gene coords and TPMs
-Rscript generate_gencode_v22_gene_TPM_bed.R
-
-# Sum gene TPMs within 1 Mbs
-bedtools map -o sum -a resource/GRCh38.p13_1Mb.bed -b resource/gencode.v22.gene.TPM.bed > resource/GRCh38.p13_1Mb_geneTPM.bed
