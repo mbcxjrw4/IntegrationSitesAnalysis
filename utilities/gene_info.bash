80432e805acfa7313a5dafac4a501aaf7a62a66c
mbcxjrw4 <xiruowang@hotmail.com>
2025-07-30 17:15:09 +0100

    Delete utilities/gene_info.bash

--- utilities/gene_info.bash
+++
@@ -1,3 +0,0 @@
-# Comprehensive gene annotation (Gencode v22, hg38)
-# get gene info from GTF file (gencode v22)
-awk '$3 == "gene" { print $1,$4,$5,$7,$10,$16,$12 }' path_to_file/gencode.v22.annotation.gtf | tr -d ";\""  > ../intermediate/gencode.v22.gene.ranges
