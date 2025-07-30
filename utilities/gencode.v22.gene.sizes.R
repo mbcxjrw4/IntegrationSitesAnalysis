6e19f871a058f09391db4422505af9c0f2744f90
mbcxjrw4 <xiruowang@hotmail.com>
2025-07-30 21:20:09 +0100

    Update gencode.v22.gene.sizes.R

--- utilities/gencode.v22.gene.sizes.R
+++ utilities/gencode.v22.gene.sizes.R
@@ -6,5 +6,5 @@ suppressWarnings(gencode.v22.txdb <- GenomicFeatures::makeTxDbFromGFF('path_to_f
 ex <- GenomicFeatures::exonsBy(gencode.v22.txdb, 'gene') # extract exons
 exSum <- sum(width(reduce(ex)))  # sum of non-overlapping exon sequence
 geneSize <- data.frame(ensembl_id = names(exSum), gene_exon_size = as.integer(exSum), stringsAsFactors = F)
-write.table(geneSize, 'intermediate/gencode.v22.gene.sizes.tsv', sep = '\t', col.names = T, row.names = F, quote = F, na = '')
+write.table(geneSize, 'gencode.v22.gene.sizes.tsv', sep = '\t', col.names = T, row.names = F, quote = F, na = '')
 pander::pander(sessionInfo())
