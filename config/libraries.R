4d1beb330e5b65095b998808642067347da6c546
mbcxjrw4 <xiruowang@hotmail.com>
2025-07-30 13:06:55 +0100

    Update libraries.R

--- config/libraries.R
+++ config/libraries.R
@@ -10,9 +10,3 @@ suppressPackageStartupMessages( {
     library(data.table)
     library(gridExtra)
 })
-
-# variables
-adaptPalette <- c(
-  "#5062AF","#BF1E2D","#6D4685","#62AF50","#E1C000","#1EBFB0","#38457B","#AA86BF","#315728","#969696",
-  "#29066B","#AF4BCE","#EB548C","#8D2D56","#FFA500","#1DE4BD","#820401","#DE542C","#EABD3B","#005D68",
-  "#8A9A5B","#C7EA46","#50C878","#98FB98","#555555","#1AC9E6")
