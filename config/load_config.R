1d315b05ce846099e269eededf08ef9c96510588
mbcxjrw4 <xiruowang@hotmail.com>
2025-09-20 08:55:41 +0100

    Update load_config.R

--- config/load_config.R
+++ config/load_config.R
@@ -1,11 +1,28 @@
-library(yaml)
+#!/usr/bin/env Rscript
 
+suppressPackageStartupMessages(library(yaml))
+suppressPackageStartupMessages(library(optparse))
+
+# Function to load YAML config
 load_config <- function(config_file = "config/config.yaml") {
   cfg <- yaml::read_yaml(config_file)
   return(cfg)
 }
 
-# Example usage inside your scripts:
-# cfg <- load_config()
+# Command line option
+option_list <- list(
+  make_option(c("-c", "--config"), type="character", default="config/config.yaml",
+              help="Path to YAML config file", metavar="character")
+)
+
+opt_parser <- OptionParser(option_list=option_list)
+opt <- parse_args(opt_parser)
+
+cfg <- load_config(opt$config)
+
+# Make cfg available globally
+assign("cfg", cfg, envir = .GlobalEnv)
+
+# Example usage inside analysis scripts:
 # patient_id <- cfg$patient_id
 # genome_fasta <- cfg$genome_fasta
