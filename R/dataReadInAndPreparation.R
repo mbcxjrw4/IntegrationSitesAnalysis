6e25e05092fb2d395dd7bc7ae68569e3b37edf8d
mbcxjrw4 <xiruowang@hotmail.com>
2025-09-23 14:03:14 +0100

    Update dataReadInAndPreparation.R

--- R/dataReadInAndPreparation.R
+++ R/dataReadInAndPreparation.R
@@ -8,16 +8,15 @@ suppressPackageStartupMessages(library(readr))
 # ---------------------------
 option_list <- list(
   make_option(c("-i", "--input"),  type="character", help="Input integration sites file (.tsv)", metavar="file"),
-  make_option(c("-o", "--outdir"), type="character", help="Output directory for intermediate files", metavar="dir"),
   make_option(c("-c", "--config"), type="character", default="config/config.yaml", help="Path to YAML config file", metavar="file")
 )
 
 opt_parser <- OptionParser(option_list=option_list)
 opt <- parse_args(opt_parser)
 
-if (is.null(opt$input) || is.null(opt$outdir)) {
+if (is.null(opt$input)) {
   print_help(opt_parser)
-  stop("Missing required arguments: --input and/or --outdir", call.=FALSE)
+  stop("Missing required arguments: --input", call.=FALSE)
 }
 
 # ---------------------------
@@ -29,10 +28,6 @@ source("config/load_config.R")  # this defines global variable `cfg`
 # Main script
 # ---------------------------
 message("Running Data Preparation...")
-message("Input file: ", opt$input)
-message("Output directory: ", opt$outdir)
-
-dir.create(opt$outdir, showWarnings = FALSE, recursive = TRUE)
 
 # Use config values
 patient_id <- cfg$patient_id
@@ -48,15 +43,14 @@ IS <- IS[IS$subject %in% patient_id, ]
 IS[, "regions"] <- apply(IS, 1, function(x) paste0(x[1], ":", as.integer(x[2])-10, "-",  as.integer(x[2])+10))
 IS$regions <- sapply(IS$regions, as.character)
 
-if (!dir.exists("intermediate")) dir.create("intermediate")
-if (!file.exists("intermediate/20bp.plus.region")) {
-	writeLines(sapply(unique(IS[IS$strand == "+", "regions"]), as.character), file.path(opt$outdir, "20bp.plus.region"))
+if (!file.exists("20bp.plus.region")) {
+	writeLines(sapply(unique(IS[IS$strand == "+", "regions"]), as.character), "20bp.plus.region")
 }
-if (!file.exists("intermediate/20bp.minus.region")) {
-	writeLines(sapply(unique(IS[IS$strand == "-", "regions"]), as.character), file.path(opt$outdir, "20bp.minus.region"))
+if (!file.exists("20bp.minus.region")) {
+	writeLines(sapply(unique(IS[IS$strand == "-", "regions"]), as.character), "20bp.minus.region")
 }
 
 # update IS file
-data.table::fwrite(IS, file = file.path(opt$outdir, "IS.csv"))
+data.table::fwrite(IS, file = "IS.csv")
 
 message("Data preparation complete ✅")
